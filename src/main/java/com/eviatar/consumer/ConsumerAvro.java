84d6cb2c4d18baeb1f3002a11ba4ca631b674332
Eviatar brigman <71985255+Eviatarbrigman@users.noreply.github.com>
2024-05-26 13:40:48 +0300

    add retry and dlt Handler

--- src/main/java/com/eviatar/consumer/ConsumerAvro.java
+++ src/main/java/com/eviatar/consumer/ConsumerAvro.java
@@ -3,20 +3,45 @@ package com.eviatar.consumer;
 import com.eviatar.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
 public class ConsumerAvro {
 
+    final String topic = "avro-topic12";
 
-//    @KafkaListener(topics = "${topic.name}")
-    @KafkaListener(topics = "avro-topic12",groupId = "avro-group")
+
+    //    @KafkaListener(topics = "${topic.name}")
+    @RetryableTopic(attempts = "4", backoff = @Backoff(delay = 3000, multiplier = 2, maxDelay = 15000), exclude = {NullPointerException.class})
+    @KafkaListener(topics = topic, groupId = "avro-group")
     public void getMessage(ConsumerRecord<String, User> consumerRecord) {
-        String key = consumerRecord.key();
-        User value = consumerRecord.value();
-        log.info("key" + key + "and value " + value );
+        try {
+            String key = consumerRecord.key();
+            User value = consumerRecord.value();
+            log.info("key" + key + "and value " + value);
+
+            List<Long> banedIdList = Stream.of(1234L, 4321L).toList();
+            Long idAsLong = Long.parseLong(consumerRecord.value().getId().toString());
+            if (banedIdList.contains(idAsLong)){
+                throw new RuntimeException("this Id is band");
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DltHandler
+    public void retryTopic(ConsumerRecord<String, User> consumerRecord) {
+
+        log.info("the issue is with topic : {} ,offset : {} and id : {}", consumerRecord.topic(), consumerRecord.offset(), consumerRecord.value().getId());
 
     }
 }
